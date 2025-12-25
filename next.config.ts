f39f5ddb5fba98d4adc7540f14679c3641582c18
Rodrigo Suarez <ranaraniada2@gmail.com>
2025-12-25 14:41:44 -0300

    fix imagenes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Requerido para exportación estática
+  },
 };
 
 export default nextConfig;
